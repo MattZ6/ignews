82dc053cf8ecb99d3b267ae8c006b89ea69db726
Matheus Felipe Zanin <matt_z6@hotmail.com>
2021-03-25 11:05:45 -0300

    feat: add Github signin

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,19 +1,21 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { signIn, signOut, useSession } from 'next-auth/client';
 
 import styles from './styles.module.scss';
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const [session] = useSession();
 
-  if (isUserLoggedIn) {
+  if (session) {
     return (
       <button
         type="button"
         className={styles.signInButton}
+        onClick={() => signOut()}
       >
         <FaGithub color="#04d361" />
-        <strong>Nome do usuário</strong>
+        <strong>{ session.user?.name }</strong>
         <FiX color="#737380" className={styles.closeIcon} />
       </button>
     );
@@ -23,6 +25,7 @@ export function SignInButton() {
     <button
       type="button"
       className={styles.signInButton}
+      onClick={() => signIn('github')}
     >
       <FaGithub color="#eba417" />
       <strong>Sign in with Github</strong>
