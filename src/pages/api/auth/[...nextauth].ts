70d1853489ce183aa0a46a3e7fcf13fc959e457e
Matheus Felipe Zanin <matt_z6@hotmail.com>
2021-03-29 19:48:14 -0300

    feat: create user on database

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
 import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
+import { query as q } from 'faunadb';
+
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   providers: [
@@ -9,4 +12,42 @@ export default NextAuth({
       scope: 'read:user'
     }),
   ],
+  callbacks: {
+    async signIn (user) {
+      console.log(user);
+
+      const { email } = user;
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(email),
+                )
+              )
+            ),
+            // then
+            q.Create(
+              q.Collection('users'),
+              { data: { email } },
+            ),
+            // else
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(email),
+              ),
+            )
+          )
+        );
+
+        return true;
+      } catch {
+        return false;
+      }
+    }
+  }
 });
